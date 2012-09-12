6d305475fc88df2dbe69c1ae169503caa4b99c2c
Vincent Scheib <scheib@gmail.com>
2012-09-12 16:51:02 -0700

    binding and looking for data

--- hello-world-udp/hello-udp.js
+++ hello-world-udp/hello-udp.js
@@ -1,5 +1,25 @@
 window.addEventListener('load', function() {
   output("onload!");
+
+  chrome.socket.create('udp', {}, function(createInfo) {
+  	bound_socket = createInfo['socketId'];
+    chrome.socket.bind(bound_socket, "0.0.0.0", 0, function(result) {
+      if (result) {
+        console.warn('could not bind udp socket', address);
+        callback(null);
+      } else {
+        output("Bound to " + bound_socket);
+      }
+    });
+  });
+
+  setInterval(tick, 250);
+  function tick() {
+  	chrome.socket.recvFrom(bound_socket, function(info) {
+  		if (info.data.byteLength)
+  			output("received: " + info.data.byteLength + " " + info.data);
+  	});
+  };
 });
 
 function output(innerHTML) {
