1a1b6a3224272ba0c5e326407d0e89f14b28d937
Vincent Scheib <scheib@gmail.com>
2012-09-13 16:35:28 -0700

    send_ping

--- hello-world-udp/hello-udp.js
+++ hello-world-udp/hello-udp.js
@@ -9,10 +9,28 @@ window.addEventListener('load', function() {
       } else {
         output("Bound to " + bound_socket);
         queue_recvFrom();
+        send_ping();
       }
     });
   });
+
+  // Development hack --- kill off all sockets!
+  document.body.onkeydown = function (e) { 
+    output("chrome.socket.destroy(bound_socket);");
+    chrome.socket.destroy(bound_socket);
+    output("chrome.socket.destroy(i);");
+    for (var i=0; i<100; i++) {
+      chrome.socket.destroy(i);
+    }
+  }
 });
+
+function output(innerHTML) {
+  output_ul = document.getElementById("output");
+  li = document.createElement("li");
+  li.innerHTML = innerHTML;
+  output_ul.appendChild(li)
+}
 
 function queue_recvFrom() {
   chrome.socket.recvFrom(bound_socket, data_received);
@@ -20,12 +38,17 @@ function queue_recvFrom() {
 function data_received(info) {
   if (info.data.byteLength)
     output("received: " + info.data.byteLength + " " + info.data);
+  console.log(info);
   queue_recvFrom();
 };
 
-function output(innerHTML) {
-  output_ul = document.getElementById("output");
-  li = document.createElement("li");
-  li.innerHTML = innerHTML;
-  output_ul.appendChild(li)
+function send_ping() {
+  var uint_array = new Uint8Array(new ArrayBuffer(1));
+  uint_array[0] = 1;
+
+  chrome.socket.sendTo(bound_socket, uint_array.buffer, '224.0.0.251', 535353, function(writeInfo) {
+    if (writeInfo.bytesWritten != uint_array.byteLength) {
+      output('could not write DNS packet.');
+    }
+  });
 }
