f492e65a0049d573031b2799e5f79b6fa713bab8
Vincent Scheib <scheib@gmail.com>
2013-03-13 16:12:11 -0700

    hello-world

--- hello-world/main.js
+++ hello-world/main.js
@@ -10,8 +10,12 @@ chrome.app.runtime.onLaunched.addListener(function() {
   var width = 500;
   var height = 300;
 
-  chrome.app.window.create('index.html',
-    {width: width, height: height, 
-     left: (screenWidth-width)/2, 
-     top: (screenHeight-height)/2});
+  chrome.app.window.create('index.html', {
+    bounds: {
+      width: width,
+      height: height,
+      left: Math.round((screenWidth-width)/2),
+      top: Math.round((screenHeight-height)/2)
+    }
+  });
 });
